d8f249807563555527815d4ee211d38362b4030d
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-14 15:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Code your solution in this file.
 const lowerCaseDrivers = drivers => drivers.map(d=>d.toLowerCase());
+const nameToAttributes = drivers => drivers.map(d=>({firstName: "", lastName: ""}));
 const attributesToPhrase = drivers => drivers.map(d=>`${d.name} is from ${d.hometown}`);
